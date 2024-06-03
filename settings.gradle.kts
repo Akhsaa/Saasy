a86ee4c3f52dc849f3ed50e4e7f4fbfc5886aa3b
Akhsaa <104117400+Akhsaa@users.noreply.github.com>
2024-06-03 21:05:34 +0700

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "UTSAndroid-PEMROGRAMAN ANDROID 3-10121273-AKHSA LAVRIL KUSUMA"
+include(":app")
+ 
